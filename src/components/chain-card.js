390c1ede804f0653d9fdc76393d47488125a5eb3
mintthemoon <mint@mintthemoon.xyz>
2023-03-08 16:23:06 -0500

    temporarily remove staking display (refreshing too often)

--- src/components/chain-card.js
+++ src/components/chain-card.js
@@ -5,20 +5,21 @@ import Card from 'react-bootstrap/Card'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 
-import FormatNumber from './format-number'
+// import FormatNumber from './format-number'
 
 const ChainCard = ({chain}) => {
-  const [stakingInfo, setStakingInfo] = React.useState({})
-  const getStakingInfo = () => {
-    fetch(`${chain.lcd}/cosmos/staking/v1beta1/validators/${chain.valoper}`)
-      .then(response => response.json())
-      .then(result => {setStakingInfo(result.validator)})
-  }
-  React.useEffect(() => {
-    getStakingInfo()
-    const interval = setInterval(() => {getStakingInfo()}, 5000)
-    return () => clearInterval(interval)
-  })
+  // const [stakingInfo, setStakingInfo] = React.useState({})
+  // const getStakingInfo = () => {
+  //   fetch(`${chain.lcd}/cosmos/staking/v1beta1/validators/${chain.valoper}`)
+  //     .then(response => response.json())
+  //     .then(result => {setStakingInfo(result.validator)})
+  //     .catch(console.log)
+  // }
+  // React.useEffect(() => {
+  //   getStakingInfo()
+  //   const interval = setInterval(() => {getStakingInfo()}, 5000)
+  //   return () => clearInterval(interval)
+  // })
 
   return (
     <Card bg="dark" text="light" style={{width: "calc(280px + 5vw)"}} className="text-center p-2 mx-auto">
@@ -36,12 +37,12 @@ const ChainCard = ({chain}) => {
               <Button variant="primary text-dark" href={chain.stake} target="_blank">Stake</Button>
             </ButtonGroup>
           </Card.Text>
-          <Card.Text className="mb-1 mt-0 text-secondary text-opacity-75">
+          {/* <Card.Text className="mb-1 mt-0 text-secondary text-opacity-75">
             <FormatNumber exponent={chain.native_assets[0].exponent} decimals="0">
               {stakingInfo.tokens}
             </FormatNumber>
             &nbsp;{chain.native_assets[0].symbol} staked!
-          </Card.Text>
+          </Card.Text> */}
         </Col>
       </Row>
     </Card>
